f6039a8c6c53a0620601429934dfc719fbe3612d
Jonathan Miller <jonathan@miller-interactive.com>
2018-06-21 20:25:53 -0500

    further vue integration

---
+++ source/assets/scripts/app.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    title: 'Website Scaffolding',
+    description: 'A good starting point for building a website with Vue.js'
+  },
+  methods: {
+
+  }
+})
